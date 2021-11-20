c2736bbf797f0fec9d9ad38bc24c333c35b3ce3d
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2021-11-19 23:29:29 -0500

    ExamenUnidad2

--- ExamenJGOM/src/main/java/pe/edu/upeu/jgom/App.java
+++ ExamenJGOM/src/main/java/pe/edu/upeu/jgom/App.java
@@ -1,7 +1,7 @@
 package pe.edu.upeu.jgom;
 
-import pe.edu.upeu.jgom.utils.LeerTeclado;//*
-import pe.edu.upeu.jgom.examen.ResolucionExamen;//*
+import pe.edu.upeu.jgom.utils.LeerTeclado;
+import pe.edu.upeu.jgom.examen.ResolucionExamen;
 
 
 public class App{
@@ -23,8 +23,8 @@ public class App{
                         
                 case 3: res.numerosperfectos();break;
 
-                case 4: int x=lt2.leer(0,"Introdusca el primer valor");
-                        int n=lt2.leer(0,"Introdusca el segundo valor");
+                case 4: int x=lt2.leer(0,"Introdusca la base");
+                        int n=lt2.leer(0,"Introdusca el exponente");
                         System.out.println("El resultado es: "+res.potencia(x, n));break;
                         
                 default: System.out.println("Elija un algoritmo existente!");   
@@ -32,7 +32,7 @@ public class App{
             }
             System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^");
             f=lt2.leer(0,"\nLista de Algoritmos:"+"\n\n-Pregunta 2 : 1"+"\n-Pregunta 3 : 2"+
-            "\n-Pregunta 4 : 3"+"\n-Pregunta 5 : 4"+"\n\nIntrodusca el numero del algoritmo que desea Probar");
+            "\n-Pregunta 4 : 3"+"\n-Pregunta 5 : 4"+"\n-Para Cerrar El menu de opciones Introdusca el Numero: 0"+"\n\nIntrodusca el numero del algoritmo que desea Probar");
 
         }
 
--- ExamenJGOM/src/main/java/pe/edu/upeu/jgom/examen/ResolucionExamen.java
+++ ExamenJGOM/src/main/java/pe/edu/upeu/jgom/examen/ResolucionExamen.java
@@ -23,25 +23,26 @@ public class ResolucionExamen {
             clave =lt1.leer(0, "ingresa la clave del auto: ");
             if (clave > 0 && clave<4){
                 if (clave ==1){
-                    impuesto = precio * 0.10f; 
+                    impuesto = (precio*12)/100; 
                     categoria1 = categoria1 + impuesto;
                     total = total + impuesto;
                 System.out.println("el impuesto a pagar por el auto: "+n+" es s/: "+impuesto);
                 }
                 if (clave ==2){
-                    impuesto = precio * 0.07f; 
+                    impuesto = (precio*8)/100; 
                     categoria2 = categoria2 + impuesto;
                     total = total + impuesto;
                 System.out.println("el impuesto a pagar por el auto: "+n+" es s/: "+impuesto);
                 }
                 if (clave ==3){
-                    impuesto = precio * 0.05f; 
+                    impuesto = (precio*5)/100; 
                     categoria3 = categoria3 + impuesto;
                     total = total + impuesto;
                 System.out.println("el impuesto a pagar por el auto: "+n+" es s/: "+impuesto);
                 }
                 //total = total + impuesto;
                 //System.out.println("el impuesto a pagar por el auto: "+n+" es s/: "+impuesto);
+                n--;
             }
             else
                 System.out.println("ingrese la clave correcta");
@@ -59,7 +60,7 @@ public class ResolucionExamen {
         int n=1;
         int m=20;
         for (int i=n; i<=m; i++){
-            for(int j=1; j<=12; j++){
+            for(int j=1; j<=10; j++){
                 System.out.println(i+" x "+j+" = "+(i*j));
             }
         }
@@ -91,5 +92,5 @@ public class ResolucionExamen {
         }
 
     }
-    
+
 }
