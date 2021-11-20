971c85333455921cf26110755b3ff290ac5bf9be
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2021-11-19 20:29:31 -0500

    ExamenUnidad2

--- ExamenJGOM/src/main/java/pe/edu/upeu/jgom/App.java
+++ ExamenJGOM/src/main/java/pe/edu/upeu/jgom/App.java
@@ -1,13 +1,41 @@
 package pe.edu.upeu.jgom;
-import pe.edu.upeu.jgom.utils.LeerTeclado;
-import pe.edu.upeu.jgom.examen.ResolucionExamen;
+
+import pe.edu.upeu.jgom.utils.LeerTeclado;//*
+import pe.edu.upeu.jgom.examen.ResolucionExamen;//*
 
 
 public class App{
-    static LeerTeclado lt = new LeerTeclado();
+    //Objetos 
+    static LeerTeclado lt2 = new LeerTeclado();
     static ResolucionExamen res = new ResolucionExamen();
-
+    
+    //Metodo MENUDEOPCIONES
     static void menuDeOpciones(){
+
+        int f=lt2.leer(0,"\nLista de Algoritmos:"+"\n\n-Pregunta 2 : 1"+"\n-Pregunta 3 : 2"+
+        "\n-Pregunta 4 : 3"+"\n-Pregunta 5 : 4"+"\n-Para Cerrar El Algoritomo Introdusca el Numero: 0"+"\n\nIntrodusca el numero del algoritmo que desea Probar");
+        
+        while(f!=0){
+            switch(f){
+                case 1: res.impuestoAutos();break;
+
+                case 2: res.TMultiplicar1hasta20();break;
+                        
+                case 3: res.numerosperfectos();break;
+
+                case 4: int x=lt2.leer(0,"Introdusca el primer valor");
+                        int n=lt2.leer(0,"Introdusca el segundo valor");
+                        System.out.println("El resultado es: "+res.potencia(x, n));break;
+                        
+                default: System.out.println("Elija un algoritmo existente!");   
+
+            }
+            System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^");
+            f=lt2.leer(0,"\nLista de Algoritmos:"+"\n\n-Pregunta 2 : 1"+"\n-Pregunta 3 : 2"+
+            "\n-Pregunta 4 : 3"+"\n-Pregunta 5 : 4"+"\n\nIntrodusca el numero del algoritmo que desea Probar");
+
+        }
+
         
     }
 
@@ -15,12 +43,9 @@ public class App{
 
     public static void main( String[] args ){
 
-        System.out.println( "Bienvenido al menú de opciónes del examen de la segunda unidad By JGOM!" );
-        int a=10;
-        
-        int b= res.multiplicacion3(a);
-
-        System.out.println(b);
+        System.out.println( "<---------Bienvenido al menú de opciónes del examen de la segunda unidad By JGOM!--------->" );
+       
+        menuDeOpciones();
        
     }
 
--- ExamenJGOM/src/main/java/pe/edu/upeu/jgom/examen/ResolucionExamen.java
+++ ExamenJGOM/src/main/java/pe/edu/upeu/jgom/examen/ResolucionExamen.java
@@ -1,11 +1,95 @@
 package pe.edu.upeu.jgom.examen;
 
-public class ResolucionExamen {
-    
-    public int multiplicacion3(int p){
-       int r = p*3;
-       return r;
-    }
+import pe.edu.upeu.jgom.utils.LeerTeclado;
 
+public class ResolucionExamen {
+    //Objetos
+    LeerTeclado lt1 =new LeerTeclado();
+    
+    //Pregunta 2:
+    public void impuestoAutos() {
+        int n, clave;
+        float precio, impuesto, categoria1, categoria2, categoria3, total;
+        total = 0;
+        categoria1 = 0;
+        categoria2 = 0;
+        categoria3 = 0;
+       
+        n =lt1.leer(0,"ingresar la cantidad de autos" );
+        while (n>0){
+            
+            precio =lt1.leer(0, "ingresa el precio del auto: "+n);
+            
+            clave =lt1.leer(0, "ingresa la clave del auto: ");
+            if (clave > 0 && clave<4){
+                if (clave ==1){
+                    impuesto = precio * 0.10f; 
+                    categoria1 = categoria1 + impuesto;
+                    total = total + impuesto;
+                System.out.println("el impuesto a pagar por el auto: "+n+" es s/: "+impuesto);
+                }
+                if (clave ==2){
+                    impuesto = precio * 0.07f; 
+                    categoria2 = categoria2 + impuesto;
+                    total = total + impuesto;
+                System.out.println("el impuesto a pagar por el auto: "+n+" es s/: "+impuesto);
+                }
+                if (clave ==3){
+                    impuesto = precio * 0.05f; 
+                    categoria3 = categoria3 + impuesto;
+                    total = total + impuesto;
+                System.out.println("el impuesto a pagar por el auto: "+n+" es s/: "+impuesto);
+                }
+                //total = total + impuesto;
+                //System.out.println("el impuesto a pagar por el auto: "+n+" es s/: "+impuesto);
+            }
+            else
+                System.out.println("ingrese la clave correcta");
+            
+        
+            }
+            System.out.println("el impuesto por la categoria 1 es s/: "+categoria1);
+            System.out.println("el impuesto por la categoria 2 es s/: "+categoria2);
+            System.out.println("el impuesto por la categoria 3 es s/: "+categoria3);
+            System.out.println("el impuesto total por todos los autos es s/: "+total);     
+        } 
+
+    //Pregunta 3:
+    public void TMultiplicar1hasta20() {
+        int n=1;
+        int m=20;
+        for (int i=n; i<=m; i++){
+            for(int j=1; j<=12; j++){
+                System.out.println(i+" x "+j+" = "+(i*j));
+            }
+        }
+    }
+    //Pregunta 4:
+    public void numerosperfectos() {
+        int i, j, suma;
+        System.out.println("Números perfectos entre 1 y 1000: ");
+        for(i=1;i <= 1000;i++){      // i es el número que vamos a comprobar
+            suma = 0;
+            for(j = 1;j < i;j++){    // j son los divisores. Se divide desde 1 hasta i-1                          
+                    if(i % j==0){
+                    suma = suma + j; // si es divisor se suma
+                    }
+            }
+            if(i == suma){             // si el numero es igual a la suma de sus divisores es perfecto              
+                System.out.println(i);
+            }
+        }
+    }
+    //Pregunta 5:
+    public int potencia(int b, int e) {
+        if (e==1) {
+            return b;
+        } if (e==0) {
+            return 1;
+        } else{
+            return b * potencia(b, e-1);
+        }
+
+    }
     
 }
